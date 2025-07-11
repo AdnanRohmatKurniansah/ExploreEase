f8c1cc673d1fe699de19d93cdebfd86f428110ff
AdnanRohmatKurniansah <atnandimas@gmail.com>
2025-07-11 14:51:11 +0700

    fix: typo

--- app/(root)/bookings/[slug]/_components/pay-tour.tsx
+++ app/(root)/bookings/[slug]/_components/pay-tour.tsx
@@ -53,7 +53,7 @@ const PayTour = ({ booking, subtotal, discount, total }: Props) => {
       onSuccess: function (result) {
         console.log('Payment success:', result)
         toast.success("Payment Success")
-        router.push(`/thaknyou/${booking.order_id}`)
+        router.push(`/thankyou/${booking.order_id}`)
       },
       onPending: function (result) {
         console.log('Payment pending:', result)
